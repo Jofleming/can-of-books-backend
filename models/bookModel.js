10918b82d428b7df421a7147a7e5a4508e2780b2
Ben Mills <fuzzyllama@gmail.com>
2021-11-10 22:49:01 -0800

    Adds styling

--- models/bookModel.js
+++ models/bookModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title: String,
   description: String,
-  status: String,
+  status: Boolean,
   email: String,
 });
 
