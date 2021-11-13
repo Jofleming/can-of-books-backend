b2858f22730de002d260b0c1e9350c23b0bb10fb
Ben Mills <fuzzyllama@gmail.com>
2021-11-13 13:11:04 -0800

    Initial auth0 setup

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 const Book = require('./models/bookModel.js');
+const verifyUser = require('./auth.js');
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('Mongoose is connected'));
@@ -18,24 +19,27 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
-const handleBookRequest = async (req, res) => {
-  let queryObj = {};
-  if (req.query.email) {
-    queryObj = {email: req.query.email};
-  }
-  try {
-    const booksFromDB = await Book.find(queryObj);
-    if (booksFromDB.length > 0) {
-      res.status(200).send(booksFromDB);
-    } else {
-      res.status(404).send('no books');
-    }
-  } catch (e) {
-    console.log(e);
-    res.status(500).send('bigger issue');
-  }
+const handleBookRequest = (req, res) => {
 
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      res.status(498).send('Token not valid.')
+    } else {
+      try {
+        const booksFromDB = await Book.find({email: user.email});
+        if (booksFromDB.length > 0) {
+          res.status(200).send(booksFromDB);
+        } else {
+          res.status(404).send('no books');
+        }
+      } catch (e) {
+        console.log(e);
+        res.status(500).send('Server error');
+      }
+    }
+  })
 };
+
 
 const handleBookPost = async (req, res) => {
   try {
@@ -81,9 +85,20 @@ const handleBookPut = async (req, res) => {
   }
 }
 
+const getUser = (req, res) => {
+  verifyUser(req, (err, user) => {
+    if (err) {
+      res.status(498).send('Token not valid');
+    } else {
+      res.status(200).send(user);
+    }
+  })
+}
+
 app.get('/books', handleBookRequest);
 app.post('/books', handleBookPost);
 app.delete('/books/:id', handleBookDelete);
 app.put('/books/:id', handleBookPut);
+app.get('/user', getUser);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
